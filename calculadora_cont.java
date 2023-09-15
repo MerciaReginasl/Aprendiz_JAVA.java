1b588c8b12a2e4ca0c65674727853772450a31d4
Mércia Regina da Silva <39158160+MerciaReginasl@users.noreply.github.com>
2023-09-15 13:59:18 -0300

    Create calculadora_cont.java

---
+++ calculadora_cont.java
@@ -0,0 +1,27 @@
+// Continuação do desafio 2
+
+package resolucoesDesafios.desafio2;
+
+public class Operacoes {
+    public int multiplicacao(int num1, int num2){
+        return num1 * num2;
+}
+
+    public int divisao(int num1, int num2) {
+        return num1 / num2;
+    }
+
+    public int adicao(int num1, int num2){
+
+        return num1 + num2;
+    }
+
+    public int subtracao(int num1, int num2){
+        return num1 - num2;
+    }
+
+    public int modulo(int num1, int num2) {
+        return num1 % num2;
+
+    }
+}
