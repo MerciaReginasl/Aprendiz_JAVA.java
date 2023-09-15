01f58146e347dc6b39b43a70428c846dcc85f56f
Mércia Regina da Silva <39158160+MerciaReginasl@users.noreply.github.com>
2023-09-15 14:04:30 -0300

    Create Numeros_Inteiros.java

---
+++ Numeros_Inteiros.java
@@ -0,0 +1,25 @@
+⏺ Desafio 3:
+Crie um programa que receba números inteiros e ao final mostre quantos números foram digitados, a quantidade de números pares e a quantidade de números ímpares, o programa continua recebendo números até que o número 0 seja digitada. O programa não recebe números negativos.
+
+Entrada:
+
+Números inteiros positivos
+
+Saída:
+
+O programa imprime na saída a quantidade de números que foram lidos e a média desses números.
+
+Observação: Esse códgio poddde ser feito diretamente na classe Main
+
+Exemplo de Execução:
+
+Informe um número: 8
+Informe um número: 9
+Informe um número: 6
+Informe um número: 7
+Informe um número: 6
+Informe um número: 0
+
+A quantiddade de números lidos foi: 5
+A quantidade de números pares foi: 3
+A quantidade de números ímpares foi: 2
